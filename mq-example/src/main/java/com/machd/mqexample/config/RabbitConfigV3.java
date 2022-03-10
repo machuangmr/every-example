486ba3b6de90550cbbf827d6797e3ebe7591407f
machd <1224352076>
2022-03-10 17:45:48 +0800

    feat: mq-抽取配置文件

--- mq-example/src/main/java/com/machd/mqexample/config/RabbitConfigV3.java
+++ mq-example/src/main/java/com/machd/mqexample/config/RabbitConfigV3.java
@@ -1,14 +1,10 @@
 package com.machd.mqexample.config;
 
-import com.sun.org.glassfish.external.statistics.annotations.Reset;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
